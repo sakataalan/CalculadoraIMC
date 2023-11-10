e00fa229965d74827b20c54261f9032035486da5
Bruno Aoto <Bruno.Aoto.Pelissari@gmail.com>
2023-11-10 16:36:49 -0300

    added onCLick and CalculateIMC method

--- app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.myapp;
+package com.example.calculadoraimc;
 
+import androidx.appcompat.R;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,10 +10,39 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("LifeCicle", "onCreate");
+
+        final EditText editTextWeight = findViewById(R.id.editTextWeight);
+        final EditText editTextHeight = findViewById(R.id.editTextHeight);
+        Button buttonCalculate = findViewById(R.id.buttonCalculate);
+        final textView textViewResult = findViewById(R.id.textViewResult);
+
+        buttonCalculate.setOnclickListener(new View.oinClickListener()
+            @Override
+            public void onClick(View v) {
+                String weightStr = editTextWeight.getText().toString();
+                String heightStr = editTextHeight.getText().toString();
+
+                if (!weightStr.isEmpty() && !heightStr.isEmpty()) {
+                    float weight = Float.parseFloat(weightStr);
+                    float height = Float.parseFloat(heightStr);
+
+                    float imc = calculateIMC(weight, height);
+
+                    textViewResult.setText(getString(R.string.result) + " " + imc);
+                } else {
+                    textViewResult.setText(getString(R.string.result) + " " + getString(R.string.calculation_error));
+                }
+            }
+        });
+    }
+
+    private float calculateIMC(float weight, float height) {
+        return weight / (height * height);
     }
 
     @Override
