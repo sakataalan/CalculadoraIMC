a1c4e209363f2350985a9a81ab8cb99589f934bb
Bruno Aoto <Bruno.Aoto.Pelissari@gmail.com>
2023-11-10 15:37:29 -0300

    added LifeCicles in MainActivity

--- app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.calculadoraimc;
+package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("LifeCicle", "onCreate");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("LifeCicle", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("LifeCicle", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("LifeCicle", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.wtf("LifeCicle", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("LifeCicle", "onDestroy");
+    }
+
 }
