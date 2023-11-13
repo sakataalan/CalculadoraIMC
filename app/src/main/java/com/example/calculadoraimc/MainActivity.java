215d5d1208d68a2b7d5dc87c4c6bf6e0dffb02fc
sakataalan <sakataalan@yahoo.com.br>
2023-11-13 16:10:09 -0300

    add toast funcion

--- app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -4,16 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editTextWeight;
     EditText editTextHeight;
-    Button buttonCalculate;
     TextView textViewResult;
 
     @Override
@@ -27,16 +28,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Float calculateIMC(Float weight, Float height) {
-        return (weight / height) * 100;
+        return (weight / (height * height)) * 10000;
     }
 
     public void calcButtonOnClick(View v) {
         Float weight = Float.parseFloat(editTextWeight.getText().toString());
         Float height = Float.parseFloat(editTextHeight.getText().toString());
 
-        Float result = calculateIMC(weight, height);
+        Float resultIMC = calculateIMC(weight, height);
+        String result = String.format("%.2f", resultIMC);
 
-        textViewResult.setText(String.format("%.2f", result));
+        textViewResult.setText(result);
+        showToast(resultIMC);
+    }
+
+    private void showToast(Float result) {
+        if (result < 18.5) {
+            Toast toast = Toast.makeText(this, "Abaixo do peso", Toast.LENGTH_LONG);
+            toast.show();
+        } else if (result >= 18.5 && result <= 24.9) {
+            Toast toast = Toast.makeText(this, "Peso normal", Toast.LENGTH_LONG);
+            toast.show();
+        } else if (result >= 25.0 && result <= 29.9) {
+            Toast toast = Toast.makeText(this, "Pré-obesidade", Toast.LENGTH_LONG);
+            toast.show();
+        } else if (result >= 30.0 && result <= 34.9) {
+            Toast toast = Toast.makeText(this, "Obesidade Grau 1", Toast.LENGTH_LONG);
+            toast.show();
+        } else if (result >= 35.0 && result <= 39.9) {
+            Toast toast = Toast.makeText(this, "Obesidade Grau 2", Toast.LENGTH_LONG);
+            toast.show();
+        } else if (result >= 40) {
+            Toast toast = Toast.makeText(this, "Obesidade Grau 3", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 
     @Override
